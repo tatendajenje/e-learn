370db691741deb25bf656bbbecd7d8ce561afd17
Tatenda Jenje <67785492+tatendajenje@users.noreply.github.com>
2022-09-01 02:27:57 +0200

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import RoutesMain from "./routes";
-
-function App() {
-  return <RoutesMain />;
-}
-
-export default App;
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import cartReducer from './redux/features/shop/cartSlice' 
-
-export const store = configureStore({
-    reducer:{
-        cartReducer,
-    }
-}) 
